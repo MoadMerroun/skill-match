958d51a6cc5efd3c19626b85dd670b5f12f08229
MoadMerroun <moadmerroun@gmail.com>
2025-10-22 23:13:15 +0100

    Add user model in accounts app

--- accounts/models.py
+++ accounts/models.py
@@ -1,1 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+ROLE_CHOICES = (
+    ( "candidate", "Candidate" ),
+    ( "employer", "Employer" )
+)
+
+class User( AbstractUser ):
+    role = models.CharField( max_length = 20, choices = ROLE_CHOICES )
+    email = models.EmailField( unique = True )
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = [ "username" ]
