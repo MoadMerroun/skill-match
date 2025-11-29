bf017fa0833f7b11df5ecd0d7de392d2326ec496
MoadMerroun <moadmerroun@gmail.com>
2025-11-29 14:45:40 +0100

    Add candidate and company profile models in profiles app

--- profiles/models.py
+++ profiles/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
-# Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(
+        "accounts.User",
+        on_delete = models.CASCADE
+    )
+
+    created_at = models.DateTimeField( auto_now_add = True )
+
+    updated_at = models.DateTimeField( auto_now = True )
+
+    class Meta:
+        abstract = True
+
+class CandidateProfile( Profile ):
+    bio = models.TextField( default = "", blank = True )
+
+    experience = models.PositiveIntegerField( default = 0, blank = True )
+
+    cv_url = models.CharField( max_length=255, default = "", blank = True )
+
+class CompanyProfile( Profile ):
+    company_name = models.CharField( default = "", blank = True, max_length = 100 )
+
+    description = models.TextField( default = "", blank = True )
+
+    website = models.URLField( default = "", blank = True )
+
+    logo_url = models.CharField( max_length=255, default = "", blank = True )
+
+    industry = models.CharField( max_length = 100, default = "", blank = True )
+
+    number_of_employees = models.PositiveIntegerField( default = 0, blank = True )
